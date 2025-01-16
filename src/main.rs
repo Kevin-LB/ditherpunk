7abc1b5c60a31dc0991b6a6ad1d502f2296109fd
Kevin-LB <144025457+Kevin-LB@users.noreply.github.com>
2025-01-16 11:00:47 +0100

    [code question 2] passage en rgb8

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use image::GenericImageView;
+use image::{DynamicImage, GenericImageView, ImageBuffer, RgbImage};
+use std::path::Path;
 
 fn main() {
-    let img = image::open("test.png").expect("Erreur");
-    println!("Dimensions: {:?}", img.dimensions());
-    println!("Color type: {:?}", img.color());
+    let img_path = Path::new("image.png");
+    
+    let img = image::open(&img_path).expect("Erreur lors de l'ouvertre de l'image");
+    let rgb_img: RgbImage = img.to_rgb8();
+
+    let output_path = Path::new("output.png");
+    rgb_img.save(output_path).expect("Erreur lors de l'enregistrement de l'image");
+
+    println!("Image convertie et sauvegardée avec succès !");
 }
