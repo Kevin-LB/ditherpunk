654f5de325fb73a3db6c1f128616066653f5c8c1
Kevin-LB <144025457+Kevin-LB@users.noreply.github.com>
2025-01-17 11:04:57 +0100

    [code question 4] couleur du pixel (32,52)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-use image::{DynamicImage, GenericImageView, ImageBuffer, RgbImage};
+use image::{DynamicImage, GenericImageView, ImageBuffer, RgbImage, Pixel};
 use std::path::Path;
 
 fn main() {
     let img_path = Path::new("image.png");
     
     let img = image::open(&img_path).expect("Erreur lors de l'ouvertre de l'image");
-    let rgb_img: RgbImage = img.to_rgb8();
+    let pixel = img.get_pixel(32, 52);
 
-    let output_path = Path::new("output.png");
-    rgb_img.save(output_path).expect("Erreur lors de l'enregistrement de l'image");
-
-    println!("Image convertie et sauvegardée avec succès !");
+    println!("La couleur du pixel (32, 52) est : {:?}", pixel.to_rgb());
 }
